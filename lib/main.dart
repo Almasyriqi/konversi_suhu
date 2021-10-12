19513cf9ec1817f61f611b4cee1ee8a721036b26
Almasyriqi <ikramalmasyriqi@gmail.com>
2021-10-12 22:26:50 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Konversi Suhu',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Konverter Suhu"),
+        ),
+        body: Container(
+          margin: const EdgeInsets.all(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Masukkan Suhu Dalam Celcius',
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  WhitelistingTextInputFormatter.digitsOnly
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(5, 30, 5, 30),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.orange)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: const [
+                            Text("Suhu dalam Kelvin"),
+                            Padding(padding: EdgeInsets.only(bottom: 20)),
+                            Text(
+                              "150",
+                              style: TextStyle(fontSize: 36),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(5, 30, 5, 30),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.orange)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: const [
+                            Text("Suhu dalam Reamor"),
+                            Padding(padding: EdgeInsets.only(bottom: 20)),
+                            Text(
+                              "200",
+                              style: TextStyle(fontSize: 36),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(0, 30, 0, 30),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.orange)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: const [
+                            Text("Suhu dalam Reamor"),
+                            Padding(padding: EdgeInsets.only(bottom: 20)),
+                            Text(
+                              "200",
+                              style: TextStyle(fontSize: 36),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 50),
+                ),
+                onPressed: () {},
+                child: const Text('Konversi Suhu'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
