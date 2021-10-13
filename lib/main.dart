64901897c4307a98524db9c2b507e880bc61c8c2
Almasyriqi <ikramalmasyriqi@gmail.com>
2021-10-13 15:30:39 +0700

    Membagi Ke Widget Yang Lebih Kecil

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:konversi_suhu/widget/input.dart';
+import 'package:konversi_suhu/widget/result.dart';
 
 void main() {
   runApp(const MyApp());
@@ -54,88 +55,22 @@ class _MyAppState extends State<MyApp> {
             children: <Widget>[
               Form(
                 key: _formKey,
-                child: TextFormField(
-                  controller: etInput,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Silahkan masukkan suhu dalam celcius';
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(
-                    hintText: 'Masukkan Suhu Dalam Celcius',
-                  ),
-                  keyboardType: TextInputType.number,
-                  inputFormatters: <TextInputFormatter>[
-                    WhitelistingTextInputFormatter.digitsOnly
-                  ],
-                ),
+                child: Input(etInput: etInput),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Column(
-                    // ignore: prefer_const_literals_to_create_immutables
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.only(left: 10),
-                        padding: const EdgeInsets.fromLTRB(5, 30, 5, 30),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.orange)),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Text("Suhu Kelvin"),
-                            const Padding(padding: EdgeInsets.only(bottom: 20)),
-                            Text(
-                              _kelvin.toStringAsFixed(2),
-                              style: const TextStyle(fontSize: 36),
-                            )
-                          ],
-                        ),
-                      ),
-                    ],
+                  Result(
+                    result: _kelvin,
+                    name: "Suhu Kelvin",
                   ),
-                  Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.fromLTRB(5, 30, 5, 30),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.orange)),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Text("Suhu Fahrenheit"),
-                            const Padding(padding: EdgeInsets.only(bottom: 20)),
-                            Text(
-                              _fahrenheit.toStringAsFixed(2),
-                              style: const TextStyle(fontSize: 36),
-                            )
-                          ],
-                        ),
-                      ),
-                    ],
+                  Result(
+                    result: _fahrenheit,
+                    name: "Suhu Fahrenheit",
                   ),
-                  Column(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.only(right: 10),
-                        padding: const EdgeInsets.fromLTRB(5, 30, 5, 30),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.orange)),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Text("Suhu Reamur"),
-                            const Padding(padding: EdgeInsets.only(bottom: 20)),
-                            Text(
-                              _reamur.toStringAsFixed(2),
-                              style: const TextStyle(fontSize: 36),
-                            )
-                          ],
-                        ),
-                      ),
-                    ],
+                  Result(
+                    result: _reamur,
+                    name: "Suhu Reamur",
                   ),
                 ],
               ),
